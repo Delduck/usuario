c07b5f6d86afa322c9bb4192a65d4fc787a1c87f
João Pedro Delduck <jpdelduck@gmail.com>
2026-05-31 12:46:34 -0300

    Adicionando metodos get,delete e login

--- src/main/java/com/delduck/usuario/business/UsuarioService.java
+++ src/main/java/com/delduck/usuario/business/UsuarioService.java
@@ -3,8 +3,11 @@ package com.delduck.usuario.business;
 import com.delduck.usuario.business.converter.UsuarioConverter;
 import com.delduck.usuario.business.dto.UsuarioDTO;
 import com.delduck.usuario.infrastructure.entity.Usuario;
+import com.delduck.usuario.infrastructure.exceptions.ConflictException;
+import com.delduck.usuario.infrastructure.exceptions.ResourceNotFoundException;
 import com.delduck.usuario.infrastructure.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,10 +16,39 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioConverter usuarioConverter;
+    private final PasswordEncoder passwordEncoder;
 
     public UsuarioDTO salvaUsuario(UsuarioDTO usuarioDTO) {
+        emailExiste(usuarioDTO.getEmail());
+        usuarioDTO.setSenha(passwordEncoder.encode(usuarioDTO.getSenha()));
         Usuario usuario = usuarioConverter.paraUsuario(usuarioDTO);
+
         return usuarioConverter.paraUsuarioDTO(usuarioRepository.save(usuario));
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return usuarioRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException("Email não encontrado " + email));
+    }
+
+    public void deletarUsuarioPorEmail(String email) {
+        usuarioRepository.deleteByEmail(email);
+    }
+
+    //apenas chamar nosso metodo na repository
+    public boolean verificaEmailExistente(String email) {
+        return usuarioRepository.existsByEmail(email);
+    }
+
+    //verificar se o email existe, caso exista, lança exception
+    public void emailExiste(String email) {
+        try{
+            if(verificaEmailExistente(email)) {
+                throw new ConflictException("Email já cadastrado " + email);
+            }
+        }catch (ConflictException e) {
+            throw new ConflictException("Email já cadastrado ",  e.getCause());
+        }
     }
 
 }
--- src/main/java/com/delduck/usuario/infrastructure/repository/TelefoneRepository.java
+++ src/main/java/com/delduck/usuario/infrastructure/repository/TelefoneRepository.java
@@ -1,6 +1,6 @@
 package com.delduck.usuario.infrastructure.repository;
 
-import com.delduck.aprendendo_spring.infrastructure.entity.Telefone;
+import com.delduck.usuario.infrastructure.entity.Telefone;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
